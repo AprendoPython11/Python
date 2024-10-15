e26e3e18ac169a1678d868a7ca0a6af3e095babb
angienohava <angienohava03>
2024-10-15 10:06:15 -0500

    the cumputer guess the number

--- guess.ramdom.number.py
+++ guess.ramdom.number.py
@@ -1,5 +1,6 @@
 import random
 
+# the user guess the random number 
 def guess(y):
     random_number = random.randint(1, y)
     guess = 0
@@ -15,5 +16,24 @@ def guess(y):
 
     print(f"Congratulacion, you did. {random_number} It was the correctly number")
 
+# the computer guess the random number 
+
+def computer_guess(y):
+    low = 1
+    hight = y
+    reply = ""
+    
+    while (reply != "c"):
+        number_for_guess = random.randint(low, hight)
+        reply = input(f"Is {number_for_guess} too low (l) or too hight(h), or maybe is correct (c)? :")
+        
+        if(reply == "l"):
+            low = number_for_guess + 1
+            
+        elif(reply == "h"):
+            hight = number_for_guess - 1
+    
+    print("I did! I guess the number {number_for_guess}")
+            
 
 guess(10)
